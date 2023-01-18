71c9e7c487f901173c9902ea1ac9abd549f13d46
manish-prakram <manishkumar@prakram.onmicrosoft.com>
2023-01-18 17:27:49 +0530

    recruiter update and get by id

--- app/routers/recruiter.py
+++ app/routers/recruiter.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from fastapi import Response, status, HTTPException, Depends, APIRouter
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from .. import utils, oauth2
 from app.models import models
 from app.schemas import schemas, recruiter_schema
@@ -11,25 +14,56 @@ router = APIRouter(
 )
 
 
-@router.get('/')
-def get_profile(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),):
+@router.get('/me')
+def get_my_profile(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),):
 
     if current_user.profileType != 2:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail=f"Not Allowed!!!")
 
-    recruiter = db.query(models.RecruiterProfile).filter(models.RecruiterProfile.owner_id ==
-                                                         current_user.id).first()  # For same user posts only
+    recruiter_query = db.query(models.RecruiterProfile).filter(models.RecruiterProfile.owner_id ==
+                                                               current_user.id)
+
+    recruiter = recruiter_query.first()
+
+    # For updating Last Login
+    recruiter_query.update({'lastLogin': datetime.now()},
+                           synchronize_session=False)
 
     if not recruiter:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Data does not exist")
-    print(recruiter)
-    return {"userData": current_user, "recruiterData": recruiter}
+    db.commit()
+    db.refresh(recruiter)
+    db.refresh(current_user)
+
+    return JSONResponse(jsonable_encoder({"userData": current_user, "recruiterData": recruiter}))
+
+
+@router.get('/{id}', response_model=recruiter_schema.RecruiterResponse)
+async def get_profile(id: int, db: Session = Depends(get_db)):
+
+    recruiter_query = db.query(models.RecruiterProfile).filter(
+        models.RecruiterProfile.id == id)
+
+    recruiter = recruiter_query.first()
+
+    if not recruiter:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Data does not exist")
+
+    return recruiter
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create_recruiter(body: recruiter_schema.CreateRecruiter, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),):
+
+    query_recruiter = db.query(models.RecruiterProfile).filter(
+        models.RecruiterProfile.owner_id == current_user.id).first()
+
+    if query_recruiter:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f'Recruiter already exist')
 
     new_recruiter = models.RecruiterProfile(
         owner_id=current_user.id, **body.dict())
@@ -39,3 +73,21 @@ def create_recruiter(body: recruiter_schema.CreateRecruiter, db: Session = Depen
     db.refresh(new_recruiter)
 
     return new_recruiter
+
+
+@router.patch('/me', status_code=status.HTTP_200_OK, response_model=recruiter_schema.RecruiterResponse)
+async def update_recruiter(body: recruiter_schema.UpdateRecruiter, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    query_recruiter = db.query(models.RecruiterProfile).filter(
+        models.RecruiterProfile.owner_id == current_user.id)
+    recruiter = query_recruiter.first()
+
+    if not recruiter:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Recruiter Does not exist!!!')
+
+    query_recruiter.update(body.dict(exclude_unset=True),
+                           synchronize_session=False)
+    db.commit()
+
+    return recruiter
--- app/schemas/recruiter_schema.py
+++ app/schemas/recruiter_schema.py
@@ -9,20 +9,36 @@ class CreateRecruiter(BaseModel):
     companyName: str
     industry: str
     noticePeriod: str
-    websiteUrl: str = None
-    linkedInUrl: str = None
-    otherUrl: str = None
-    profilePicUrl: str = None
-    bannerPicUrl: str = None
-    hiringRoleTitle: str = None
-    hiringRoleDescription: str = None
+    websiteUrl: Optional[str] = None
+    linkedInUrl: Optional[str] = None
+    otherUrl: Optional[str] = None
+    profilePicUrl: Optional[str] = None
+    bannerPicUrl: Optional[str] = None
+    hiringRoleTitle: Optional[str] = None
+    hiringRoleDescription: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class UpdateRecruiter(BaseModel):
+    jobTitle: Optional[str]
+    companyName: Optional[str]
+    industry: Optional[str]
+    noticePeriod: Optional[str]
+    websiteUrl: Optional[str]
+    linkedInUrl: Optional[str]
+    otherUrl: Optional[str]
+    profilePicUrl: Optional[str]
+    bannerPicUrl: Optional[str]
+    hiringRoleTitle: Optional[str]
+    hiringRoleDescription: Optional[str]
 
     class Config:
         orm_mode = True
 
 
 class RecruiterResponse(BaseModel):
-    id: int
     jobTitle: str
     companyName: str
     industry: str
@@ -34,8 +50,6 @@ class RecruiterResponse(BaseModel):
     bannerPicUrl: str = None
     hiringRoleTitle: str = None
     hiringRoleDescription: str = None
-    owner_id: str
-    lastLogin: datetime
 
     class Config:
         orm_mode = True
